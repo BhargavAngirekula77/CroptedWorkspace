87387248fabdd97d902ac517e462bd0cde2ccb9f
angir <angir@192.168.1.16>
2023-11-17 13:46:13 +0530

    commit and push

---
+++ src/advance_xpath/QETGEA.java
@@ -0,0 +1,11 @@
+package advance_xpath;
+
+public class QETGEA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("uhuiubiubobob");
+
+	}
+
+}
